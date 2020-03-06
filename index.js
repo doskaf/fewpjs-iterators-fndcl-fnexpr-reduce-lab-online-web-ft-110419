69bb33456331ef7a3cca5df747293bfde68f8c2b
Dominique Skaf <doskaf@yahoo.com>
2020-03-06 00:52:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 function totalBatteries(batteries) {
-  batteries.reduce(function(total, element){ return element + total}, 0);
+  return batteries.reduce(function(total, element){ return element + total}, 0);
 }
